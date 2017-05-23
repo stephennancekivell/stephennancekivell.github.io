3b2b1fb23260c7259baa5ded950aea4b9ec04f20
Stephen Nancekivell <stephen.nancekivell@gmail.com>
2017-05-23 20:00:38 +1000

    hide profile in mobile

--- release.sh
+++ release.sh
@@ -2,4 +2,4 @@
 
 JEKYLL_ENV=production jekyll build
 aws --profile sn s3 sync ./_site s3://blog.stephenn.com
-aws --profile sn cloudfront create-invalidation --distribution-id E3ANG6POYB4SIY --paths '/*'
+aws --profile sn cloudfront create-invalidation --distribution-id E1DRN1L45UMGVD --paths '/*'
