f57d27c4a1b2665f1a03f3e1ada560b8ae912471
Stephen Nancekivell <stephen.nancekivell@gmail.com>
2017-06-11 20:59:16 +1000

    icon

--- release.sh
+++ release.sh
@@ -2,4 +2,5 @@
 
 JEKYLL_ENV=production jekyll build
 aws --profile sn s3 sync ./_site s3://blog.stephenn.com
-aws --profile sn cloudfront create-invalidation --distribution-id E1DRN1L45UMGVD --paths '/*'
+aws --profile sn cloudfront create-invalidation --distribution-id EZBKNZ3I0201S --paths '/*'
+
