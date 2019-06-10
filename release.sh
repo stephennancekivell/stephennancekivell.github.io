3730dec7fef73dd2b745e54cdbe3a50abb4d8954
Stephen Nancekivell <stephen.nancekivell@gmail.com>
2019-06-10 10:51:33 +1000

    upgrade jekyll to 3.8.5

--- release.sh
+++ release.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-JEKYLL_ENV=production jekyll build
-aws --profile sn s3 sync ./_site s3://blog.stephenn.com
 aws --profile sn cloudfront create-invalidation --distribution-id EZBKNZ3I0201S --paths '/*'
-
